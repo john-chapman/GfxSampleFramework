50b360c726c6b76ec7ebbc750f9609f042c58d57
John <j.x.chapman@gmail.com>
2017-06-01 18:54:21 +0200

    Sampling: Cleanup, added Bayer functions

--- data/common/shaders/Sampling.glsl
+++ data/common/shaders/Sampling.glsl
@@ -28,19 +28,47 @@ vec2 Sampling_Hammersley2d(in uint _i, in float _rn)
 // Return a uniformly-distributed point on the unit hemisphere oriented along +Z given a random _uv (e.g. the result of Sampling_Hammersley2d).
 vec3 Sampling_Hemisphere_Uniform(in vec2 _uv) 
 {
-	float phi = _uv.y * 2.0f * kPi;
-	float ct = 1.0f - _uv.x;
-	float st = sqrt(1.0f - ct * ct);
+	float phi = _uv.y * 2.0 * kPi;
+	float ct = 1.0 - _uv.x;
+	float st = sqrt(1.0 - ct * ct);
+	return vec3(cos(phi) * st, sin(phi) * st, ct);
+}
+
+// Return a cosine-distributed point on the unit hemisphere oriented along +Z given a random _uv (e.g. the result of Sampling_Hammersley2d).
+vec3 Sampling_Hemisphere_Cosine(in vec2 _uv)
+{
+	float phi = _uv.y * 2.0 * kPi;
+	float ct = sqrt(1.0 - _uv.x);
+	float st = sqrt(1.0 - ct * ct);
 	return vec3(cos(phi) * st, sin(phi) * st, ct);
 }
 
 // Return a point on the unit hemisphere oriented along +Z given a random _uv, skewed by _a (0 = +Z, 1 = uniform).
 vec3 Sampling_Hemisphere(in vec2 _uv, in float _a) 
 {
-	float phi = _uv.y * 2.0f * kPi;
+	float phi = _uv.y * 2.0 * kPi;
 	float ct = sqrt((1.0 - _uv.y) / (1.0 + (_a * _a - 1.0) * _uv.y ));
-	float st = sqrt(1.0f - ct * ct);
+	float st = sqrt(1.0 - ct * ct);
 	return vec3(cos(phi) * st, sin(phi) * st, ct);
+}
+
+
+// \todo ALU only versions?
+float Bayer_2x2(in uvec2 _seed)
+{
+	_seed.x = _seed.x % 2;
+	_seed.y = _seed.y % 2;
+	uint i = _seed.x + _seed.y * 2;
+	const float kBayer[] = { 0.25, 0.75, 1.0, 0.5 };
+	return kBayer[i];
+}
+float Bayer_4x4(in uvec2 _seed)
+{
+	_seed.x = _seed.x % 4;
+	_seed.y = _seed.y % 4;
+	uint i = _seed.x + _seed.y * 4;
+	const float kBayer[] = { 0.0625, 0.5625, 0.1875, 0.6875, 0.8125, 0.3125, 0.9375, 0.4375, 0.25, 0.75, 0.125, 0.625, 1.0, 0.5, 0.875, 0.375 };
+	return kBayer[i];
 }
 
 #endif // Sampling_glsl
