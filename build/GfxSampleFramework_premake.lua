cbb0188bb8d6231942c8761dfdc992e619093d01
John <j.x.chapman@gmail.com>
2017-08-02 16:52:09 +0200

    Premake/ApplicationTools fixes

--- build/GfxSampleFramework_premake.lua
+++ build/GfxSampleFramework_premake.lua
@@ -5,19 +5,21 @@ local ALL_EXTERN_DIR  = ALL_SRC_DIR .. "extern/"
 local WIN_SRC_DIR     = SRC_DIR .. "win/"
 local WIN_EXTERN_DIR  = WIN_SRC_DIR .. "extern/"
 
-local function GfxSampleFramework_Common(_root)
+local function GfxSampleFramework_SetPaths(_root)
 	SRC_DIR         = _root .. SRC_DIR
 	ALL_SRC_DIR     = _root .. ALL_SRC_DIR
 	ALL_EXTERN_DIR  = _root .. ALL_EXTERN_DIR
 	WIN_SRC_DIR     = _root .. WIN_SRC_DIR
 	WIN_EXTERN_DIR  = _root .. WIN_EXTERN_DIR
+end
 
+local function GfxSampleFramework_Globals()
 	project "*"
-	
+
 	defines { "GLEW_STATIC" }
 	rtti "Off"
 	exceptionhandling "Off"
-	
+
 	includedirs({
 		ALL_SRC_DIR,
 		ALL_EXTERN_DIR,
@@ -30,18 +32,19 @@ local function GfxSampleFramework_Common(_root)
 	filter {}
 end
 
-function GfxSampleFramework_Project(_root, _targetDir)
+function GfxSampleFramework_Project(_root, _libDir, _binDir)
 	_root = _root or ""
-	_targetDir = _targetDir or "../lib"
-	
-	GfxSampleFramework_Common(_root)
-	
+	_libDir = _libDir or "../lib"
+	_binDir = _binDir or "../bin"
+
+	GfxSampleFramework_SetPaths(_root)
+
 	project "GfxSampleFramework"
 		kind "StaticLib"
 		language "C++"
-		targetdir(_targetDir)
+		targetdir(_libDir)
 		uuid(FRM_UUID)
-			
+
 		filter { "configurations:debug" }
 			targetsuffix "_debug"
 			symbols "On"
@@ -52,56 +55,56 @@ function GfxSampleFramework_Project(_root, _targetDir)
 			symbols "Off"
 			optimize "Full"
 		filter {}
-			
+
 		vpaths({
 			["*"]        = ALL_SRC_DIR .. "frm/**",
-			["extern/*"] = ALL_EXTERN_DIR .. "**", 
+			["extern/*"] = ALL_EXTERN_DIR .. "**",
 			["win"]      = WIN_SRC_DIR .. "frm/**",
 			})
-			
-		files({ 
-			ALL_SRC_DIR    .. "**.h", 
-			ALL_SRC_DIR    .. "**.hpp", 
-			ALL_SRC_DIR    .. "**.c", 
-			ALL_SRC_DIR    .. "**.cpp", 
-			ALL_EXTERN_DIR .. "**.c", 
-			ALL_EXTERN_DIR .. "**.cpp", 
+
+		files({
+			ALL_SRC_DIR    .. "**.h",
+			ALL_SRC_DIR    .. "**.hpp",
+			ALL_SRC_DIR    .. "**.c",
+			ALL_SRC_DIR    .. "**.cpp",
+			ALL_EXTERN_DIR .. "**.c",
+			ALL_EXTERN_DIR .. "**.cpp",
 			})
 		removefiles({
 			ALL_EXTERN_DIR .. "lua/lua.c", -- standalone lua interpreter
 			ALL_EXTERN_DIR .. "lua/luac.c",
 			})
 		filter { "platforms:Win*" }
-			files({ 
+			files({
 				WIN_SRC_DIR .. "**.h",
 				WIN_SRC_DIR .. "**.hpp",
 				WIN_SRC_DIR .. "**.c",
 				WIN_SRC_DIR .. "**.cpp",
 			})
 		filter {}
-			
+
 		filter { "action:vs*" }
 			postbuildcommands({
-				"rmdir \"$(ProjectDir)..\\..\\bin\\common\"",
-				"mklink /j \"$(ProjectDir)..\\..\\bin\\common\" " .. "\"$(ProjectDir)..\\..\\data\\common\"",
+				"rmdir \"$(ProjectDir)../" .. _binDir .. "/common\"",
+				"mklink /j \"$(ProjectDir)../" .. _binDir .. "/common\" " .. "\"$(ProjectDir)../" .. _root .. "/data/common\"",
 				})
 		filter {}
-		
-	project "*"
+
+	GfxSampleFramework_Globals()
 end
 
 function GfxSampleFramework_ProjectExternal(_root)
 	_root = _root or ""
 
-	GfxSampleFramework_Common(_root)
+	GfxSampleFramework_SetPaths(_root)
 
 	externalproject "GfxSampleFramework"
 		location(_root .. "build/" .. _ACTION)
 		uuid(APT_UUID)
 		kind "StaticLib"
 		language "C++"
-		
-	project "*"
+
+	GfxSampleFramework_Globals()
 end
 
 function GfxSampleFramework_Link()
