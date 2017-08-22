9bea3fa7c20ab762a2b7f769e030999a2b14af43
John Chapman <contact@john-chapman.net>
2017-08-22 22:44:14 +0200

    ColorCorrection: Re-arranged color grading ops

--- data/common/shaders/ColorCorrection_fs.glsl
+++ data/common/shaders/ColorCorrection_fs.glsl
@@ -216,11 +216,10 @@ void main()
 		ret *= exp2(bfData.m_exposure);
 	#endif
 
- 	ret = Tonemap(ret);
- 	
-	ret = Saturation(ret);
- 	ret = Contrast(ret);
  	ret *= bfData.m_tint;
+ 	ret = Contrast(ret);
+ 	ret = Tonemap(ret);
+	ret = Saturation(ret); // post-tonemap to prevent hue shifts
 
  // \todo film grain?
 	//float rnd = Rand_FloatMantissa(Rand_Hash(uint(gl_FragCoord.x * 698.0 + gl_FragCoord.y) + Rand_Hash(uTime)));
