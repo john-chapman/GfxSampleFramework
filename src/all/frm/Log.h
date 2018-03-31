0522684c57ef0b405b4a28ecc3944cd1fe241e33
John <j.x.chapman@gmail.com>
2018-03-31 18:29:47 +0200

    Log: Misc cleaning

--- src/all/frm/Log.h
+++ src/all/frm/Log.h
@@ -3,40 +3,52 @@
 #include <frm/def.h>
 
 #include <apt/log.h>
-#include <apt/static_initializer.h>
 #include <apt/String.h>
 #include <apt/Time.h>
 
-#include <EASTL/bonus/ring_buffer.h>
-
 namespace frm {
 
+////////////////////////////////////////////////////////////////////////////////
+// Log
+// Message buffer with optional output file. Messages are stored in a ringbuffer
+// and flush to the output file on overflow.
+////////////////////////////////////////////////////////////////////////////////
 class Log
 {
 public:
-	typedef apt::String<64> Str;
+	typedef apt::String<64> String;
 	typedef apt::LogType    Type;
 	typedef apt::Timestamp  Timestamp;
 
 	struct Message
 	{
-		Str       m_str;
+		String    m_str;
 		Type      m_type;
 		Timestamp m_time;
 	};
 
-	Log(int _maxMessageCount, const char* _output = "");
+	// Allocate space for _bufferSize messages, optionally set the output file path.
+	Log(int _bufferSize, const char* _outputPath = "");
 	~Log();
 	
+	// Add a message to the buffer.
+	void           addMessage(const char* _msg, Type _type = apt::LogType_Count);
 
-	void           setOutput(const char* _output);
-	const Message* getLastMessage(Type _type) const;
+	// Return the last message of _type, or the back of the buffer by default.
+	const Message* getLastMessage(Type _type = apt::LogType_Count) const;
+	// Clear the cached ptr for _type.
 	void           clearLastMessage(Type _type = apt::LogType_Count);
-	void           addMessage(const char* _str, Type _type = apt::LogType_Count);
-	int            getMessageCount() const;
-	const Message* getMessage(int _i) const;
+	
+	// Flush to the output file.
 	void           flush();
 
+	// Message access.
+	int            getMessageCount() const;
+	const Message* getMessage(int _i) const;
+
+	// Set the output file.
+	void           setOutput(const char* _path);
+	
 
 private:
 	Message* m_lastMessage[apt::LogType_Count] = {};
